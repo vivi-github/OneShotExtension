b46011863da757d3d74fd236feade0bf98a5976b
vivi <vivi-github@users.noreply.github.com>
2019-11-05 18:09:28 +0900

    Popup to Sidebar

--- getPagesSource.js
+++ getPagesSource.js
@@ -1,53 +1,51 @@
 // @author Rob W <http://stackoverflow.com/users/938089/rob-w>
 // Demo: var serialized_html = DOMtoString(document);
 
-// function DOMtoString(document_root) {
-//     var html = '',
-//         node = document_root.firstChild;
-//     while (node) {
-//         switch (node.nodeType) {
-//         case Node.ELEMENT_NODE:
-//             html += node.outerHTML;
-//             break;
-//         case Node.TEXT_NODE:
-//             html += node.nodeValue;
-//             break;
-//         case Node.CDATA_SECTION_NODE:
-//             html += '<![CDATA[' + node.nodeValue + ']]>';
-//             break;
-//         case Node.COMMENT_NODE:
-//             html += '<!--' + node.nodeValue + '-->';
-//             break;
-//         case Node.DOCUMENT_TYPE_NODE:
-//             // (X)HTML documents are identified by public identifiers
-//             html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-//             break;
-//         }
-//         node = node.nextSibling;
-//     }
-//     return html;
-// }
+function DOMtoString(document_root) {
+    var html = '',
+        node = document_root.firstChild;
+    while (node) {
+        switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            html += node.outerHTML;
+            break;
+        case Node.TEXT_NODE:
+            html += node.nodeValue;
+            break;
+        case Node.CDATA_SECTION_NODE:
+            html += '<![CDATA[' + node.nodeValue + ']]>';
+            break;
+        case Node.COMMENT_NODE:
+            html += '<!--' + node.nodeValue + '-->';
+            break;
+        case Node.DOCUMENT_TYPE_NODE:
+            // (X)HTML documents are identified by public identifiers
+            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+            break;
+        }
+        node = node.nextSibling;
+    }
+    return html;
+}
 
 
 
 function getURL(document_root){
 
-
-    var URL = "";
+    var URL = '';
     
-    URL += document.getElementsByClassName("__se_btn_local se_btn_fileDown se_local")[0];
+    URL += document.getElementsByClassName("se-file-save-button __se_link")[0];
+
+   //URL += document.getElementsByTagName("a")[2];
 
     return URL;
-
-
-
 }
 
 
 
-chrome.runtime.sendMessage({
+whale.runtime.sendMessage({
     action: "getSource",
-   // source: DOMtoString(document)
+    //source: DOMtoString(document)
     source : getURL(document)
 });
 
--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener(function(request, sender) {
+whale.runtime.onMessage.addListener(function(request, sender) {
     if (request.action == "getSource") {
       message.innerText = request.source;
     }
@@ -7,20 +7,25 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
   function onWindowLoad() {
   
     var message = document.querySelector('#message');
-  
-    chrome.tabs.executeScript(null, {
+    whale.tabs.executeScript(null, {
       file: "getPagesSource.js"
     }, function() {
       // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-      if (chrome.runtime.lastError) {
-        message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+      if (whale.runtime.lastError) {
+        message.innerText = 'There was an error injecting script : \n' + whale.runtime.lastError.message;
       }
     });
   
   }
   
-  window.onload = onWindowLoad;
 
+  whale.sidebarAction.onClicked.addListener(function (){
+
+    document.readyState = onWindowLoad();
+
+  });
+
+  
 
   
 
